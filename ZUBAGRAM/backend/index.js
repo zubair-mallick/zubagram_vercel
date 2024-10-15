dee4f216b5aebee5233249b104279f58409b8115
zubair mallik <zubair7035@gmail.com>
2024-10-15 17:05:30 +0530

    cors fix

--- ZUBAGRAM/backend/index.js
+++ ZUBAGRAM/backend/index.js
@@ -26,6 +26,63 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+ 
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+    origin: [process.env.URL, "http://localhost:5173"],
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+// yha pr apni api ayengi
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
+
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get("*", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// })
+
+
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server listen at port ${PORT}`);
+});
+
+// Set headers globally for all routes
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+
 // yha pr apni api ayengi
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
